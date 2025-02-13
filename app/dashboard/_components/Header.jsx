58e0ad9f07feb9e9aa6d4954bd3c254c8b1c548d
Abdulrehman Butt <buttrehman594@gmail.com>
2025-02-13 16:40:14 +0500

    FYP Update

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation';
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link';
 
 function Header() {
 
@@ -16,6 +17,14 @@ function Header() {
             <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
 
             <ul className='hidden md:flex gap-6'>
+
+                <Link href={'/'}>
+                    <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+                    ${path == `/` && `text-primary font-bold`}
+                    `}>
+                        Home
+                    </li>
+                </Link>
 
                 <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
                     ${path == `/dashboard` && `text-primary font-bold`}
